e597f422d3a2d3417509afcfda06b9537c3fb26e
Jeremy Cantrell <jmcantrell@gmail.com>
2010-04-28 21:07:05 -0400

    Switched to using setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+        name='igrep',
+        version='0.9.0',
+        description='Find image files by attributes like dimension and aspect ratio'
+        author='Jeremy Cantrell',
+        author_email='jmcantrell@gmail.com',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            ],
+        install_requires=[
+            'PIL',
+            'ImageUtils',
+            'ScriptUtils',
+            ],
+        entry_points={
+            'console_scripts': [
+                'igrep=igrep:main',
+                ]
+            },
+        py_modules=[
+            'mimemail',
+            ],
+        )
