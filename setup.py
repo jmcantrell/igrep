d8a15528bd3e3c4f79789ceb747852a9b890f668
Jeremy Cantrell <jmcantrell@gmail.com>
2019-10-30 00:11:58 -0500

    Change formatting to meet python style recommendations

--- setup.py
+++ setup.py
@@ -3,31 +3,39 @@
 from setuptools import setup
 
 setup(
-        name='igrep',
-        version='0.9.4',
-        description='Find image files by attributes like dimension and aspect ratio.',
-        author='Jeremy Cantrell',
-        author_email='jmcantrell@gmail.com',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Environment :: Console',
-            'Intended Audience :: End Users/Desktop',
-            'License :: OSI Approved :: BSD License',
-            'Natural Language :: English',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            ],
-        install_requires=[
-            'PIL',
-            'ImageUtils',
-            'ScriptUtils',
-            ],
-        entry_points={
-            'console_scripts': [
-                'igrep=igrep:main',
-                ]
-            },
-        py_modules=[
-            'igrep',
-            ],
-        )
+
+    name='igrep',
+    version='0.9.4',
+
+    description='Find image files by attributes like dimension and aspect ratio.',
+
+    author='Jeremy Cantrell',
+    author_email='jmcantrell@gmail.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+
+    install_requires=[
+        'pil',
+        'imageutils',
+        'scriptutils',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'igrep=igrep:main',
+        ]
+    },
+
+    py_modules=[
+        'igrep',
+    ],
+
+)
